8990696057add8192dc20a0194fcf0c1f86c182d
Ten Mutunhire <tmutunhire@gmail.com>
2014-07-17 18:51:20 -0400

    Parse atoms

--- parser.hs
+++ parser.hs
@@ -6,10 +6,37 @@ import Text.ParserCombinators.Parsec hiding (spaces)
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
--- parse 
+-- parse white-space
+spaces :: Parser ()
+spaces = skipMany1 space
+
+-- data type to hold Lisp values
+data LispVal = Atom String
+	      | List [LispVal]
+	      | DottedList [LispVal] LispVal
+	      | Number Integer
+	      | String String
+	      | Bool Bool
+
+-- strings	      
+parseString :: Parser LispVal
+parseString = do char '"'
+		 x <- many (noneOf "\"")
+		 char '"'
+		 return $ String x
+
+-- parse Scheme variables
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+	       rest <- (letter <|> digit <|> symbol)
+	       let atom = [first] ++ rest
+	       return $ case atom of
+			    "#t" -> Bool True
+			    "#f" -> Bool False
+			    otherwise -> Atom atom
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value: " ++ show val 
     
