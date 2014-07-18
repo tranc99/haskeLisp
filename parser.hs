f0b6edf34214a8a387b2a2d7c78fa7abad4aba56
Ten Mutunhire <tmutunhire@gmail.com>
2014-07-17 21:33:44 -0400

    parse more Scheme syntax

--- parser.hs
+++ parser.hs
@@ -40,17 +40,41 @@ parseAtom = do first <- letter <|> symbol
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $many1 digit
 
--- parse a string | number | atom
+	
+-- parse lists (this is Lisp after all!)
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+-- parse the dotted-list
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+    
+-- parse Scheme single-quote
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
+-- parse a string | number | atom | etc
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
 	<|> parseString
 	<|> parseNumber
-
+	<|> parseQuoted
+	<|> do char '('
+	       x <- (try parseList) <|> parseDottedList
+	       char ')'
+	       return x
+    
 -- read in an expression			    
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
-    Right val -> "Found value: " ++ show val 
+    Right _ -> "Found value" 
     
 main :: IO ()
 main = do args <- getArgs
