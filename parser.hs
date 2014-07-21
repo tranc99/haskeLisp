708e02e6c654fc62be54e2d7815aa1d74959892d
Ten Mutunhire <tmutunhire@gmail.com>
2014-07-21 14:59:40 -0400

    add support for mathematical functions

--- parser.hs
+++ parser.hs
@@ -98,6 +98,23 @@ eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+-- apply
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+--primitives support
+primitives :: String [ (String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+              
+
     
 main :: IO ()
 main = getArgs >>= putStrLn . show . eval . readExpr . (!! 0)
