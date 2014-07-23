6d585dbd12165345fd2a3141a00e36919c7105b4
Ten Mutunhire <tmutunhire@gmail.com>
2014-07-22 21:52:42 -0400

    implement REPL

--- parser.hs
+++ parser.hs
@@ -2,6 +2,7 @@
 module Main where
 import Monad
 import System.Environment
+import System.IO
 import Control.Monad.Error
 import Text.ParserCombinators.Parsec hiding (spaces)
 
@@ -254,6 +255,39 @@ equal [arg1, arg2] = do
       eqvEquals <- eqv [arg1, arg2]
       return $ Bool $ (primitiveEquals || let (Bool x) = eqvEquals in x)
 equal badArgList = throwError $ NumArgs 2 badArgList
+
+
+-- flush stream
+flushStr :: String -> IO ()
+flushStr str = putStr str >>hFlush stdout
+
+-- print out a prompt, read line of input
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+-- evaluate input trap error
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+--evaluate string and print result
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+-- loop to parse input
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+   result <- prompt
+   if pred result
+      then return ()
+      else action result >> until_ pred prompt action
+      
+-- implement REPL
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
+
+
+
 
 
 
